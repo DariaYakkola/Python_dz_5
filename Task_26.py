aa91e91bdda156179449986d79b6e1c1d619df47
Daria Yakkola <yakkolabest@gmail.com>
2023-04-26 16:13:01 +0300

    Исправила опечатку

--- Task_26.py
+++ Task_26.py
@@ -1,6 +1,6 @@
 # Напишите программу, которая на вход принимает два числа A и B,
 # и возводит число А в целую степень B с помощью рекурсии.
-def degree(number1, number2, result) -> int:
+def degree(number1, number2, result=1) -> int:
     """Возводит первое число в степень второго числа"""
     if number2 == 0:
         return result
@@ -11,5 +11,4 @@ def degree(number1, number2, result) -> int:
 
 a = int(input("Введите число а: "))
 b = int(input("Введите число b: "))
-print(f"Число {a} в степени {b} - {degree(a, b, 1)}")
-
+print(f"Число {a} в степени {b} - {degree(a, b)}")
